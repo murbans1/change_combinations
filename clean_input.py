20c48298fba6187bc43cab9bc51de841add9ab6a
Maciek Urbański <maciej.u@webinterpret.com>
2018-07-25 21:24:38 +0200

    Fixup data cleaner - .14 case

--- clean_input.py
+++ clean_input.py
@@ -3,11 +3,11 @@ import re
 
 def clean_input(input):
     data = input
-    data.replace('.', '.')
+    data.replace(',', '.')
     decimal_string = re.findall('\d*\.?\d+', data)
     if decimal_string:
         return decimal_string[0]
 
     decimal_string = re.findall('\.\d+', data)
     if decimal_string:
-        return decimal_string[0]
+        return '0' + decimal_string[0]
