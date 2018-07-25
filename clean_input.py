1fb27631e558986f746b459ebf5b9f00ead7fe6e
Maciek Urbański <maciej.u@webinterpret.com>
2018-07-25 20:35:10 +0200

    Add sipmle method cleaning input strings

---
+++ clean_input.py
@@ -0,0 +1,13 @@
+import re
+
+
+def clean_input(input):
+    data = input
+    data.replace('.', '.')
+    decimal_string = re.findall('\d*\.?\d+', data)
+    if decimal_string:
+        return decimal_string[0]
+
+    decimal_string = re.findall('\.\d+', data)
+    if decimal_string:
+        return decimal_string[0]
